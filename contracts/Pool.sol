344df346b1ab3b23772fc3c01ec3a49832a5e148
witehound <netvid00@gmail.com>
2023-03-12 18:24:32 +0100

    added constructor

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -4,4 +4,14 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Pool {
     using SafeMath for uint256;
+    using SafeMath for uint32;
+    mapping(address => uint256) public balances;
+
+    uint32 public slope;
+    uint256 public totalSupply;
+
+    constructor(uint256 _initialSupply, uint32 _initialSlope) {
+        totalSupply = _initialSupply;
+        slope = _initialSlope;
+    }
 }
