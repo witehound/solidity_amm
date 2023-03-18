9103f22c107d0d27f805384e13efc3e42780ff16
“witehound” <netvid00@gmail.com>
2023-03-18 16:45:21 +0100

    debugging

--- test/Pool.js
+++ test/Pool.js
@@ -8,22 +8,27 @@ describe("Pool", () => {
     const initialSupply = ethers.utils.parseUnits("20", 8);
     const slope = 1;
     const pool = await Pool.deploy(initialSupply, slope);
+
     const tokenPrice = await pool.calculateTotalPrice();
     console.log("token price", tokenPrice);
 
     await pool.buy({ value: ethers.utils.parseEther("20.0") });
     const newTokenPrice = await pool.calculateTotalPrice();
     console.log("new token price", newTokenPrice);
-    const balance = await pool.balances(owner.address);
-    console.log("curr token bal", balance);
 
-    await pool.sell(balance);
+    const contractBalance = await ethers.provider.getBalance(pool.address);
+    console.log("initial pool bal", contractBalance);
 
-    const priceAfterSales = await pool.calculateTotalPrice();
-    console.log("price after sales", priceAfterSales);
+    // const balance = await pool.balances(owner.address);
+    // console.log("curr token bal", balance);
 
-    const balance2 = await pool.balances(owner.address);
-    console.log("bal after sales", balance2);
+    // await pool.sell(balance);
+
+    // const priceAfterSales = await pool.calculateTotalPrice();
+    // console.log("price after sales", priceAfterSales);
+
+    // const balance2 = await pool.balances(owner.address);
+    // console.log("bal after sales", balance2);
   });
 });
 
